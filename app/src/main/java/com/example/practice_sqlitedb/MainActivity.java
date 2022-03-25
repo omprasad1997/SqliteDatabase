7da4dd1287c8f2c8e07e7d56896623f7f5cc0bd7
omprasad1997 <20rathodomprasad@gmail.com>
2022-03-25 19:53:39 +0530

    Insert data(contact) in db

--- app/src/main/java/com/example/practice_sqlitedb/MainActivity.java
+++ app/src/main/java/com/example/practice_sqlitedb/MainActivity.java
@@ -15,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MyDbHandler db = new MyDbHandler(MainActivity.this);
+
+        //creating contact object for db
+        Contact harry = new Contact();
+        harry.setPhoneNumber("2113245656");
+        harry.setName("Harry");
+
+        //adding contact to db
+        db.addContact(harry);
+
+        Contact omprasad = new Contact();
+        omprasad.setPhoneNumber("43413245656");
+        omprasad.setName("Omprasad");
+
+        //adding contact to db
+        db.addContact(omprasad);
+
+        Contact pushpak = new Contact();
+        pushpak.setPhoneNumber("7843245656");
+        pushpak.setName("Pushpak");
+
+        //adding contact to db
+        db.addContact(pushpak);
+
+
     }
 }
